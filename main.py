68e0c4aad64dd78fc57a6a05b7b1c2442e8767e5
nestoras156 <107134209+nestoras156@users.noreply.github.com>
2022-06-22 17:35:17 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title('Title has been placed here')
+st.subheader('A nice header here')
+st.write(''' Stuff in multiple
+         lines go here
+         Enjoy!''')
+         
+         
